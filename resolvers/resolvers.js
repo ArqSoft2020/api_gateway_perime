a3da087e7a6675b9fcc477278df2ef20a28b1e74
kigama <kavanegasm@unal.edu.co>
2020-04-19 18:40:23 -0500

    Update resolvers.js

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,19 +1,31 @@
 // Hardcoded data store
 import {files, getFile, getFileRegister, getFileImage} from './multimedia_ms/multimedia_querys'
 import {storeFile, updateFile, deleteFile} from './multimedia_ms/multimedia_mutations'
+import {categorias, productos, getCategoria, getProducto} from './busqueda_ms/busqueda_querys'
+import {postCategoria, putCategoria, deleteCategoria, postProducto, putProducto, deleteProducto } from './busqueda_ms/busqueda_mutations'
 
 const resolvers = {
     Query: {
         files,
         getFile,
         getFileRegister,
-        getFileImage
+        getFileImage,
+        categorias, 
+        productos, 
+        getCategoria, 
+        getProducto 
     },
 
     Mutation: {
         storeFile,
         updateFile,
-        deleteFile
+        deleteFile,
+        postCategoria, 
+        putCategoria, 
+        deleteCategoria, 
+        postProducto, 
+        putProducto, 
+        deleteProducto
     }
 }
 
