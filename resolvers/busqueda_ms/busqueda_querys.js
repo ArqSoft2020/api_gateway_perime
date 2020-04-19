e24f1c88cd576d0365be248ca083fb8e8f416b13
kigama <kavanegasm@unal.edu.co>
2020-04-19 18:38:20 -0500

    Update busqueda_querys.js

--- resolvers/busqueda_ms/busqueda_querys.js
+++ resolvers/busqueda_ms/busqueda_querys.js
@@ -11,12 +11,12 @@ export const productos = async (_, args, ctx) => {
     return await axios.get(`http://${ctx.BUSQUEDA_URL}:${ctx.BUSQUEDA_PORT}/${ctx.BUSQUEDA_BASE_ENDPOINT}/productos`).then(res => res.data)    
 }
 
-/* GET ITEM categoria BY ID IN DATABASE */
+/* GET ITEM producto BY ID IN DATABASE */
 export const getCategoria= async (_, {id}, ctx) => {
     return await axios.get(`http://${ctx.BUSQUEDA_URL}:${ctx.BUSQUEDA_PORT}/${ctx.BUSQUEDA_BASE_ENDPOINT}/categoria/${id}`).then(res => res.data)
 }
-/* GET ITEM categoria BY ID IN DATABASE */
-export const getCategoria= async (_, {id}, ctx) => {
+/* GET ITEM producto BY ID IN DATABASE */
+export const getProducto= async (_, {id}, ctx) => {
     return await axios.get(`http://${ctx.BUSQUEDA_URL}:${ctx.BUSQUEDA_PORT}/${ctx.BUSQUEDA_BASE_ENDPOINT}/producto/${id}`).then(res => res.data)
 }
 
