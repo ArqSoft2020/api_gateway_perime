2886c39add9eb8c1c4404c5d3bcb76f91248609a
miagomezch <miagomezch@unal.edu.co>
2020-04-22 11:05:24 -0500

    update schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,17 +1,21 @@
 import { gql } from "apollo-server";
+import {ubication_schema, ubication_queries, ubication_mutations} from './ubication_schema/ubication_schema'
 import {multimedia_schema, multimedia_querys, multimedia_mutations} from './multimedia_schema/multimedia_schema'
 
 const schema = gql`
 
     type Query {
         ${multimedia_querys}
+        ${ubication_queries}
     }
 
     type Mutation {
         ${multimedia_mutations}
+        ${ubication_mutations}
     }
 
     ${multimedia_schema}
+    ${ubication_schema}
 `;
 
 export default schema
