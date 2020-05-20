2d292c6decfa17b6360fff4a58d64cea35eceb99
Alejandro Avila <luaavilagu@unal.edu.co>
2020-05-20 16:37:53 -0500

    Token validation verified

--- resolvers/user_ms/user_querys.js
+++ resolvers/user_ms/user_querys.js
@@ -10,28 +10,18 @@ export const users = async (_, {}, ctx) => {
 /* GET USER BY ID */
 export const getUser = async (_, {id}, ctx) => {
 
-    console.log("getUser");
-    console.log("ctx.token: " + ctx.token);
     
-    const flagValidated = await axios.get(`http://localhost:5000/perime-user-ms/validate/1`, {"headers": {"Authorization": `Bearer ${ctx.token}`}})
+    const flagValidated = await axios.get(`http://${ctx.USER_URL}:${ctx.USER_PORT}/${ctx.VALIDATE_BASE_ENDPOINT}/${id}`, { headers: { 'Authorization': `Bearer ${ctx.token}`}})
         .then(answer => {
-            //console.log("answer.status" + answer.status);
-	        return true
+            console.log("-----------------AAAnswer")
+            console.log(answer.status)
         }).catch(error => {
-                console.log(error.status);
-            }
-        )
-        ;
+                console.log("-----------------------------------Error")
+                console.log(error);
+        })
+    console.log(flagValidated)
 
-    /*
-    if(flagValidated){
-        console.log("Valid!")
-        return await axios.get(`http://${ctx.USER_URL}:${ctx.USER_PORT}/${ctx.USER_BASE_ENDPOINT}/${id}`,).then(res => res.data);
-    }
-    else
-    {
-        console.log("Valid!");
-    }
-    */
+    return  await axios.get(`http://${ctx.USER_URL}:${ctx.USER_PORT}/${ctx.USER_BASE_ENDPOINT}/${id}`,).then(res => res.data);
+   
 
 }
