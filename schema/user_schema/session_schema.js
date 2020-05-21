4629e62f0c55b3f0f7aa2309c84d95137fbfbf58
Alejandro Avila <luaavilagu@unal.edu.co>
2020-05-21 16:01:18 -0500

    Return token in delete session

--- schema/user_schema/session_schema.js
+++ schema/user_schema/session_schema.js
@@ -9,15 +9,11 @@ export const session_schema = `
         email: String
         password: String
     }
-
-    type NoSale {
-        Nada: String
-    }
 `
 
 export const session_mutations = `
     sessionmLogin(input: SessionInput): Token
-    sessionmLogout(id: Int): NoSale
+    sessionmLogout(id: Int): Token
     sessionwLogin(input: SessionInput): Token
-    sessionwLogout(id: Int): NoSale
+    sessionwLogout(id: Int): Token
 `
