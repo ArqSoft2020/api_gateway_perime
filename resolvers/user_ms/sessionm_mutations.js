b26edf563ee910bf12ac79b01a3b968005278721
daandradec <daandradec@unal.edu.co>
2020-05-22 00:05:32 -0500

    session mutation register

--- resolvers/user_ms/sessionm_mutations.js
+++ resolvers/user_ms/sessionm_mutations.js
@@ -4,7 +4,7 @@ import {createUser} from './user_mutations';
 
 /* LOGIN USER BY EMAIL AND PASSWORD IN MOVIL*/
 export const sessionmLogin = async (_, {input}, ctx) => {
-    const data = JSON.stringify(input);
+    const data = JSON.stringify(input);    
     return await axios.post(`http://${ctx.USER_URL}:${ctx.USER_PORT}/${ctx.SESSION_MOVIL_BASE_ENDPOINT}`, data, {"headers": {"Content-Type": "application/json"}}).then(res => res.data)
 }
 
@@ -15,10 +15,11 @@ export const sessionmLogout = async (_, {id}, ctx) => {
 
 export const sessionmRegister = async (_, {input}, ctx) => {
     try{
-        const user = await createUser(_, input, ctx);
-        let data = JSON.stringify(input);
-        data = {email:data.email_user, password:data.passhash_user}
-        return sessionmLogin(_, data, ctx)
+        const user = await createUser(_, {input}, ctx);        
+        if(user.id_user == undefined)
+            return new Error('Error en el Servidor :'+user);
+        const data = {input: {email:input.email_user, password:input.passhash_user}}        
+        return await sessionmLogin(_, data, ctx)
     }catch{
         return new Error('Error en el Servidor en el apigateway! vaya y mire que paso<<');
     }
