2c142a41a8ce2cbb0ad09b0511f3f46490c5c8ce
Ubuntu <ubuntu@ip-172-31-53-10.ec2.internal>
2020-05-23 01:49:21 +0000

    change resolvers

--- resolvers/search_ms/search_querys.js
+++ resolvers/search_ms/search_querys.js
@@ -4,19 +4,19 @@ import axios from 'axios';
 
 /* GET ALL categorys */
 export const categorys = async (_, args, ctx) => {    
-    return await axios.get(`http://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}categorys`).then(res => res.data)    
+    return await axios.get(`https://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}categorys`).then(res => res.data)    
 }
 /* GET ALL products */
 export const products = async (_, args, ctx) => {    
-    return await axios.get(`http://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}products`).then(res => res.data)    
+    return await axios.get(`https://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}products`).then(res => res.data)    
 }
 
 /* GET ITEM producto BY ID IN DATABASE */
 export const getCategory= async (_, {id}, ctx) => {
-    return await axios.get(`http://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}category/${id}`).then(res => res.data)
+    return await axios.get(`https://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}category/${id}`).then(res => res.data)
 }
 /* GET ITEM producto BY ID IN DATABASE */
 export const getProduct= async (_, {id}, ctx) => {
-    return await axios.get(`http://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}product/${id}`).then(res => res.data)
+    return await axios.get(`https://${ctx.SEARCH_URL}:${ctx.SEARCH_PORT}/${ctx.SEARCH_BASE_ENDPOINT}product/${id}`).then(res => res.data)
 }
 
